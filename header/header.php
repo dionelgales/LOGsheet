3e22d236e46a4f9961bd0cfd769eb5b3fa5c1cd4
dionelgales <dionelgales@gmail.com>
2018-04-11 15:02:47 +0800

    update

---
+++ header/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Material Design for Bootstrap fonts and icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+
+    <!-- Material Design for Bootstrap CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css">
+
+    <!-- Custom Style -->
+    <link rel="stylesheet" type="text/css" href="../css/custom-style.css"></li>
+
+	<title>try try</title>
+
+</head>
+
+<body>
