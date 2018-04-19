6f58aafcdb6b3134a701764302c2da1e480d71c1
dionelgales <dionelgales@gmail.com>
2018-04-20 06:15:57 +0800

    update

---
+++ client/dashboard.php
@@ -0,0 +1,66 @@
+<?php require 'header/header.php' ?>
+
+	<div class="wrapper">
+
+		<!-- left side -->
+		<div class="left-side">
+		
+		</div>
+
+		<!-- right side -->
+		<div class="right-side">
+			<div class="container-fluid cont">
+				<nav class="navbar navbar-toggleable-md navbar-dark bg-dark">
+
+					<a class="navbar-brand" href="#">Navbar</a>
+
+				</nav>
+			</div> <!-- container fluid -->
+
+			<div class="right-content">
+				<div class="row">
+
+					<div class="col-md-3">
+						<div class="card">
+							<div class="card-header"><h4>ONE</h4></div>
+							<div class="card-body">
+						    	<p class="card-text">MAN HOUR 13.50</p>
+						  	</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="card">
+							<div class="card-header"><h4>TWO</h4></div>
+							<div class="card-body">
+						    	<p class="card-text">MAN HOUR 13.50</p>
+						  	</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="card">
+							<div class="card-header"><h4>THREE</h4></div>
+							<div class="card-body">
+						    	<p class="card-text">MAN HOUR 13.50</p>
+						  	</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="card">
+							<div class="card-header"><h4>FOUR</h4></div>
+							<div class="card-body">
+						    	<p class="card-text">MAN HOUR 13.50</p>
+						  	</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		
+
+	</div>
+
+<?php require 'footer/footer.php' ?>
---
+++ client/header/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <!-- font roboto -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    
+    <!-- custom style -->
+    <link rel="stylesheet" type="text/css" href="css/custom-style.css">
+
+</head>
+
+<body>
